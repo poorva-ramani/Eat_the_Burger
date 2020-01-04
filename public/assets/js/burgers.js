0e0ecfc16599d11dae2aeb59e276d8704d9add81
Poorva Ramani <poorva.ramani@washpost.com>
2020-01-04 01:02:04 -0600

    adding everything

---
+++ public/assets/js/burgers.js
@@ -0,0 +1,45 @@
+ // Make sure we wait to attach our handlers until the DOM is fully loaded.
+$(function() {
+    $(".change-status").on("click", function(event) {
+      var id = $(this).data("id");
+      var newDevour = $(this).data("devoured");
+  
+      var newDevourState = {
+        devoured: newDevour
+      };
+  
+      // Send the PUT request.
+      $.ajax("/api/burger/" + id, {
+        type: "PUT",
+        data: newDevourState
+      }).then(
+        function() {
+          console.log("changed status to", newDevour);
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  
+    $(".create-form").on("submit", function(event) {
+      // Make sure to preventDefault on a submit event.
+      event.preventDefault();
+  
+      var newBurger = {
+        burger_name: $("#ca").val().trim()
+      };
+  
+      // Send the POST request.
+      $.ajax("/api/burger/", {
+        type: "POST",
+        data: newBurger
+      }).then(
+        function() {
+          console.log("created new Burger");
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  });
+  
